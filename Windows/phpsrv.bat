fcd988a0ab025cde1e24634664c67bfc824880c7
Fredrik Leemann <fredrik@leemann.se>
2019-01-27 18:26:29 +0100

    Updated and tested Windows phpsrv.bat

--- Windows/phpsrv.bat
+++ Windows/phpsrv.bat
@@ -1,32 +1,35 @@
 @echo OFF
 setlocal EnableDelayedExpansion
-title PHP Server Script v2.1.0
-#
-# Author: Fredrik Leemann
-# GitHub: https://github.com/freddan88/dev-scripts
-#
-# Config:
+title PHP Server Script v2.1.1 - Windows
+
+REM Author: Fredrik Leemann
+REM GitHub: https://github.com/freddan88/dev-scripts
+
+REM Config:
 set my_port=8000
-set lan_int=LAN1
-set php_exe=C:\xampp\php\php
+set lan_int=Ethernet
+set php_exe=C:\MAMP\bin\php\php7.2.10\php
 set browser=chrome
 
-REM --------------------
+REM --------------
 echo.
 netsh interface ip show addresses "%lan_int%" | find "IP" > ipv4.log
 
-for /f "usebackq tokens=1,2 delims= " %%a in (ipv4.log) do (
-	set soc=%%b
+for /f "usebackq tokens=1,2 delims=:" %%a in (ipv4.log) do (
+	for /f "usebackq tokens=1,2 delims= " %%c in ('echo %%b') do (
+		set soc=%%d
+	)
 )
 
 echo Server listening on:
 echo --------------------
-del ipv4.log
 echo.
 
+del ipv4.log
 echo %soc%:%my_port%
 echo 127.0.0.1:%my_port%
 echo localhost:%my_port%
 
+echo.
 start %browser% http://%soc%:%my_port%
 %php_exe% -S 0.0.0.0:%my_port%
