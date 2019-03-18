d3c7adf1e58e16c99f4eecd9b989c8bde9152314
Fredrik Leemann <fredrik@leemann.se>
2019-03-18 22:05:39 +0100

    added templates

---
+++ Templates/react/src/components/App/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>ReactApp</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
